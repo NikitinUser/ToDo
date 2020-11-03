988ff691b2c2ec0485aff842edd1dc1eed198d31
root <root@debian.Debian>
2020-11-03 23:34:00 +0500

    change interface

--- src/js/index.js
+++ src/js/index.js
@@ -34,15 +34,6 @@ Vue.component('task-list', {
 Vue.component('task-item', {
   template: '#task-item',
   props: ['task'],
-  computed: {
-    className() {
-      let classes = ['btn btn-secondary btn-sm col-md-11 col-sm-11'];
-      if (this.task.completed) {
-        classes.push('btn btn-success btn-sm col-md-11 col-sm-11');
-      }
-      return classes.join(' ');
-    }
-  }
 });
 
 let app = new Vue({
@@ -57,37 +48,8 @@ let app = new Vue({
       } catch(e) {
         localStorage.removeItem('tasks');
       }
-      //this.tasks = localStorage.tasks;
     }
   },
-  /*
-  watch: {
-    tasks(newTask) {
-      localStorage.tasks.push(newTask);// = newName;
-    }
-  },*/
   flag_rewrite: false,
 
 });
-/*
-document.addEventListener('DOMContentLoaded', function(){
-  axios({
-    method: 'post',
-    url: '/files/todo-checklist/ajax.php',
-    data: {
-      action: 'get-storage'
-    }
-  })
-  .then(function (response) {
-    data = response.data;
-    if (data !== 'error'){
-      app.tasks = data;
-    } else {
-      alert('Ошибка: не удалось загрузить данные');
-    }
-  })
-  .catch (function (){
-    alert('Ошибка: не удалось загрузить данные');
-  })
-});
-*/
